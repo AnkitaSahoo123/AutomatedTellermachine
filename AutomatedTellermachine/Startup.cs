ee459e3596d425efa632e82d9fc05e990c7c94e6
v-ansaho <v-ansaho@MTP-V-ANSAHO>
2017-11-27 12:47:37 +0530

    Add project files.

---
+++ AutomatedTellermachine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutomatedTellermachine.Startup))]
+namespace AutomatedTellermachine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
